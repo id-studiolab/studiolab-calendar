aed37a5a00bc1fc85f4048e5211e7d780f32323a
Carlos Precioso <kalprestito@gmail.com>
2019-11-04 18:33:25 +0100

    Comments for data.js

--- data.js
+++ data.js
@@ -2,11 +2,22 @@
 
 /** @type {import("./types/data").InputData} */
 const data = {
+  // Year for the calendar
   year: 2020,
+
+  // Language for the month and day names
   language: "en-GB",
+
+  // Each row's time will increment the previous by this amount of minutes
   row_increment_mins: 15,
+
+  // The hours that are shown in each page
   shown_hours: { from: "8:00", to: "21:00" },
+
+  // The hours that are highlighted in each page
   active_hours: [{ from: "9:00", to: "20:00" }],
+
+  // Days that will be faded out, allows `"DD/MM/YYYY"` format, or `{from: "DD/MM/YYYY", to: "DD/MM/YYYY"}`
   holidays: [
     { from: "3/2/2020", to: "7/2/2020" },
     "10/4/2020",
